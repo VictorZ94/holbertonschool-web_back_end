288b07ea79794b80f6bdcd88b937f4a5ad77d65f
Victor Zuluaga <zrvictor@outlook.com>
2021-07-19 20:08:49 -0500

    safe first element type annotated

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" complex type-annotated to Any
+"""
+from typing import Any, Union
+
+
+def safe_first_element(lst: Any) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
