09ae1b29df0d190ff6599e8acfb2190dc18c1c1a
Victor Zuluaga <zrvictor@outlook.com>
2021-07-19 20:39:42 -0500

    safe get value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" complex type-annotated to Any
+""" complex type-annotated to TypeVar
 """
 from typing import Any, TypeVar, Union, Mapping
 
@@ -10,6 +10,8 @@ T = TypeVar('T')
 def safely_get_value(dct: Mapping,
                      key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
+    """ complex type-annotated to TypeVar
+    """
     if key in dct:
         return dct[key]
     else:
