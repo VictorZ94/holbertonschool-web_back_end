070e9ae641988b4521916cb33cb2502b4d0301ae
Victor Zuluaga <zrvictor@outlook.com>
2021-07-28 13:47:45 -0500

    Create a simple pagination with CSV file

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -43,6 +43,6 @@ class Server:
         assert page > 0 and page_size > 0
         offset = self.index_range(page, page_size)
         dataset = self.dataset()
-        if offset[0] > len(dataset) and offset[1] > len(dataset):
+        if offset[0] > len(dataset) or offset[1] > len(dataset):
             return []
         return [dataset[i] for i in range(offset[0], offset[1])]
