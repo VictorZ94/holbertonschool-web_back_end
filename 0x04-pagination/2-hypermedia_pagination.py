05c8e197c6ffe2821a47ba39007a2f1eb061628d
Victor Zuluaga <zrvictor@outlook.com>
2021-07-28 15:11:55 -0500

    Hypermedia pagination

---
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+"""
+
+
+import csv
+import math
+from typing import List, Tuple, Dict, Any
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> Tuple:
+        """ tuple of range to filter
+        """
+        start_index: int = (page - 1) * page_size
+        end_index: int = page * page_size
+        tuple_of_page = (start_index, end_index)
+        return tuple_of_page
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get page and information in it
+        return list with all content filtered
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        offset = self.index_range(page, page_size)
+        dataset = self.dataset()
+        if offset[0] >= len(dataset) or offset[1] >= len(dataset):
+            return []
+        return [dataset[row] for row in range(offset[0], offset[1])]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        data_hyper: Dict = {}
+        data_hyper['page_size'] = page_size
+        data_hyper['page'] = page
+        data = self.get_page(page, page_size)
+        data_hyper['data'] = data
+
+        next_page: Any = page + 1
+        if next_page >= page:
+            next_page = None
+        data_hyper['next_page'] = next_page
+
+        prev_page: Any = page - 1
+        if prev_page < 1:
+            prev_page = None
+        data_hyper['prev_page'] = prev_page
+
+        data_hyper['total_pages'] = math.ceil(len(self.dataset()) / page_size)
+        return data_hyper
