14b7502470452534a6dcb28beadba69ee26e2d98
Victor Zuluaga <zrvictor@outlook.com>
2021-08-02 20:42:26 -0500

    Log formatter

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python3
-"""log message obfuscated:
+""" log message obfuscated
 """
 import re
 from typing import List
+import logging
 
 
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """return string obfuscated
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
     """
-    for field in fields:
 
-        message = re.sub(f"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}", message)
-    return message
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return self.filter_datum(self.fields,
+                                 self.REDACTION,
+                                 super(RedactingFormatter,
+                                       self).format(record),
+                                 self.SEPARATOR)
+
+    def filter_datum(self, fields: List[str], redaction: str,
+                     message: str, separator: str) -> str:
+        """return string obfuscated
+        """
+        for field in fields:
+            message = re.sub(f"{field}=.*?{separator}",
+                             f"{field}={redaction}{separator}", message)
+        return message
