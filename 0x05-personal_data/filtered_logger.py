6191b5d86d60e9e102cea02eca2e2a16221b2fa2
Victor Zuluaga <zrvictor@outlook.com>
2021-08-02 20:52:47 -0500

    Redacting Formatter class

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -15,10 +15,14 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ Redacting Formatter class
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Log formatter
+        """
         return self.filter_datum(self.fields,
                                  self.REDACTION,
                                  super(RedactingFormatter,
