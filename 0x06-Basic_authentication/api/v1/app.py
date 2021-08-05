accc979f0d7a303e109df9c33154ffefbbc24d2b
Victor Zuluaga <zrvictor@outlook.com>
2021-08-04 22:00:34 -0500

    setting 403 forbidden HTTP status

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -28,6 +28,13 @@ def unauthorized(unauthorized) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden(forbidden) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": f"{forbidden}"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
