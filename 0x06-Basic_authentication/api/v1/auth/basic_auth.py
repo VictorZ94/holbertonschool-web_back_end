bfbd976889e15bed64a82f3cb19476d37a948260
Victor Zuluaga <zrvictor@outlook.com>
2021-08-05 21:44:56 -0500

    setting basic authentication

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,15 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Implement a class for basic authentication
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ It returns the Base64 part of the Authorization
+        """
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith("Basic "):
+            return None
+        else:
+            return authorization_header.split()[1]
