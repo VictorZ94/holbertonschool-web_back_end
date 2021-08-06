db8fe513d3d969106400b8c24c7af242141056d8
Victor Zuluaga <zrvictor@outlook.com>
2021-08-05 20:15:10 -0500

    Implement control authorization

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -13,6 +13,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+if getenv("AUTH_TYPE") == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -33,6 +38,20 @@ def forbidden(forbidden) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_method():
+    list_path = ['/api/v1/status/', '/api/v1/unauthorized/',
+                 '/api/v1/forbidden/']
+    if auth is None:
+        return
+    if not auth.require_auth(request.path, list_path):
+        return
+    if auth.authorization_header(request) is None:
+        return abort(401)
+    if auth.current_user(request) is None:
+        return abort(403)
 
 
 if __name__ == "__main__":
