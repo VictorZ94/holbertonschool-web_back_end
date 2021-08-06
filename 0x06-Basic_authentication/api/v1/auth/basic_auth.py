c67c1215ee0eed98a6ae90a0e229ca266e5633b2
Victor Zuluaga <zrvictor@outlook.com>
2021-08-06 17:45:05 -0500

    decode args to convert from base64 to utf-8

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Implement a class for basic authentication
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ it returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        elif not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            b64decode(base64_authorization_header)
+        except Exception:
+            return None
+        return str(b64decode(base64_authorization_header), 'utf-8')
