24370a71a78ff3a8b171ed8990fd48e70ee3380a
Victor Zuluaga <zrvictor@outlook.com>
2021-08-10 16:15:04 -0500

    Create session ID by user ID

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -13,7 +13,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """ Create session ID by user ID
         """
-        if user_id is None and not isinstance(user_id, str):
+        if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
