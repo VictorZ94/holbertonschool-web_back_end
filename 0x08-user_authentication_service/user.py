3a2ee730784356c0059e1868b8ef833e2dfedf0f
Victor Zuluaga <zrvictor@outlook.com>
2021-08-15 13:38:30 -0500

    define models for user table

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -9,10 +9,12 @@ Base = declarative_base()
 
 
 class User(Base):
+    """ define first model user into database table
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
-    session_id = Column(String)
-    reset_token = Column(String)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
