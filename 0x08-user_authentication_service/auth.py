2060f82e0cb1b85e3e44486ffa6f7e7c76e1f807
Victor Zuluaga <zrvictor@outlook.com>
2021-08-15 16:19:08 -0500

    Auth class to interact with the authentication database.

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,7 +2,9 @@
 """ create password encrypted
 """
 import bcrypt
-
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 def _hash_password(password: str) -> bytes:
     """create password encrypted
@@ -10,3 +12,22 @@ def _hash_password(password: str) -> bytes:
     psw = password.encode('utf-8')
     hashed = bcrypt.hashpw(psw, bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register a new user 
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists.")
+        except NoResultFound:
+           psw = _hash_password(password)
+           new_user = self._db.add_user(email, psw)
+           return new_user
