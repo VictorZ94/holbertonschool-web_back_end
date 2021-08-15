0b0dba8dc7aa0bd4a312ea40c3d0ff57865247a5
Victor Zuluaga <zrvictor@outlook.com>
2021-08-15 17:29:18 -0500

    register user and check it

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 """ set up a basic Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'], strict_slashes=True)
+@app.route("/", methods=['GET'], strict_slashes=False)
 def welcome():
     """Create simple route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, pwd)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
