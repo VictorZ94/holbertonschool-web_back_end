2b117c842de70b30b081ee58639462660e552fd3
Victor Zuluaga <zrvictor@outlook.com>
2021-08-15 13:29:19 -0500

    implement DB

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,62 @@
+"""DB module
+"""
+# modules sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import update
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+# import modules done by me
+from user import Base
+from user import User
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **keyword: str) -> User:
+        try:
+            query = self._session.query(User).filter_by(**keyword)
+        except Exception:
+            raise InvalidRequestError
+        user = query.first()
+        if user is None:
+            raise NoResultFound
+        else:
+            return user
+
+    def update_user(self, user_id: int, **keyword) -> None:
+        user = self.find_user_by(id=user_id)
+        for k, v in keyword.items():
+            if k in user.__dict__:
+                setattr(user, k, v)
+            else:
+                raise ValueError
+        self._session.commit()
+        return None
