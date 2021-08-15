dd3632192a174ea2a2055ef5e3770596e3de8dd4
Victor Zuluaga <zrvictor@outlook.com>
2021-08-15 13:42:47 -0500

    define class DB y connection

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,4 +1,4 @@
-"""DB module
+""" DB module models and connection db
 """
 # modules sqlalchemy
 from sqlalchemy import create_engine
@@ -13,10 +13,10 @@ from sqlalchemy.exc import InvalidRequestError
 from user import Base
 from user import User
 
-class DB:
-    """DB class
-    """
 
+class DB:
+    """DB module models and connection db
+    """
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
@@ -35,12 +35,16 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """ create and add new use inside db
+        """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
 
     def find_user_by(self, **keyword: str) -> User:
+        """ search user by argument argument
+        """
         try:
             query = self._session.query(User).filter_by(**keyword)
         except Exception:
@@ -52,6 +56,8 @@ class DB:
             return user
 
     def update_user(self, user_id: int, **keyword) -> None:
+        """ update user by arbitrary argument
+        """
         user = self.find_user_by(id=user_id)
         for k, v in keyword.items():
             if k in user.__dict__:
