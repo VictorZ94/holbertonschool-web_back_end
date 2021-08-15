afa42fc622f282a4ba9a76242c72a2a4a9d55ba6
Victor Zuluaga <zrvictor@outlook.com>
2021-08-15 14:13:02 -0500

    define class DB y connection

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs: str) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """ search user by argument argument
         """
         try:
