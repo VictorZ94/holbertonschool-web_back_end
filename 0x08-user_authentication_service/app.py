ee055d4e94ed89998c3239d0457b61acd8598066
Victor Zuluaga <zrvictor@outlook.com>
2021-08-16 18:24:42 -0500

    Destroy session and redirect

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,7 +4,6 @@
 from flask import (
                    Flask, jsonify, request, abort,
                    make_response,
-                   redirect
 )
 from auth import Auth
 
@@ -58,7 +57,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect("/")
+        return "/"
     else:
         abort(403)
 
