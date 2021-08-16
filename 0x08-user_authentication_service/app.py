c8cf490d9adeb446a165c1bd6f70781582e3130d
Victor Zuluaga <zrvictor@outlook.com>
2021-08-16 18:43:44 -0500

    Destroy session and to redirect to root

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -56,7 +56,7 @@ def logout():
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user or session_id:
+    if user and session_id:
         AUTH.destroy_session(user.id)
         return redirect("/")
     else:
