b081769e175a3098a460931757a69c881f575b3d
Victor Zuluaga <zrvictor@outlook.com>
2021-08-16 19:28:44 -0500

    update reset token

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -88,12 +88,12 @@ class Auth:
         self._db.update_user(user.id, session_id=None)
         return None
 
-    def get_reset_password_token(email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """ update reset token
         """
         try:
             user = self._db.find_user_by(email=email)
-        except NoResultFound:
+        except Exception:
             raise ValueError
         identifier = _generate_uuid()
         self._db.update_user(user.id, reset_token=identifier)
