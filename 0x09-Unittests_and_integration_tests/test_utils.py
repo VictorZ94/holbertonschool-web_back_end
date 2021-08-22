4be4c74bd6e59da6af9fdf4222df0d11df1f9a1e
Victor Zuluaga <zrvictor@outlook.com>
2021-08-22 12:41:04 -0500

    patterns testing mock and fixtures

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-""" getting started write test using methoology TDD
+""" getting started write test using methoology TDD,
+    it has 3 steps fail, pass, refactor
 """
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ unit test is the first step in TDD
+        it has 3 steps fail, pass, refactor
     """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -18,3 +21,33 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         from utils import access_nested_map
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, expected):
+        """ test assertion raises from exceptions
+        """
+        from utils import access_nested_map
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """ getting started working with mock testing
+        mock some class or object is important to save resources
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ mock testing to test http GET requests
+        """
+        from utils import get_json
+        with patch("requests.get") as mock_get:
+            mock_get.return_value.json.return_value = test_payload
+            response = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(response, test_payload)
