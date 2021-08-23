c869909accffe47c1b295d4447937208b5c63ec6
Victor Zuluaga <zrvictor@outlook.com>
2021-08-23 16:20:18 -0500

    Final mock testing in client

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -74,3 +74,18 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(mock_repos_url(), 'repos_url')
             mock_repos_url.assert_called_once()
             mock_org.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ test the initial parameters
+
+        Args:
+            repo ([type]): [description]
+            license_key ([type]): [description]
+            expected ([type]): [description]
+        """
+        ret = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(ret, expected)
