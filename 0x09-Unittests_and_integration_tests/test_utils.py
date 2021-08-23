1e333ceb7ecba91d353b298f7ed26f88ad16937d
Victor Zuluaga <zrvictor@outlook.com>
2021-08-23 18:03:04 -0500

    test utils parameterized

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-""" getting started write test using methoology TDD,
-it has 3 steps fail, pass, refactor"""
+"""test utils module"""
+
 import unittest
-from utils import access_nested_map
+from unittest import mock
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ unit test is the first step in TDD
-    it has 3 steps fail, pass, refactor"""
+    """class to test access
+    nested map method"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), (1)),
@@ -16,5 +17,5 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """test a function using pattern parameterized"""
+        """test access nested map method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
