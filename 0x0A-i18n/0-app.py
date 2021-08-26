0df6fba5a0ed8bae5d6901840861f4f3cd18e47a
Victor Zuluaga <zrvictor@outlook.com>
2021-08-26 17:42:50 -0500

    imple message of welcoming

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ Simple message of welcoming
+Description:
+    to start working models i18n and l10n
 """
 from flask import Flask, render_template
 
@@ -10,11 +12,10 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """ first index template to getting started translate
+    Args:
+        template rendering
     """
-    title = 'Welcome to Holberton'
-    header = 'Hello world'
-    greeting = {'title': title, 'header': header}
-    return render_template('0-index.html', greeting=greeting)
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
