9237dde42d2839f5495ba4ef9c1652db578ef435
Victor Zuluaga <zrvictor@outlook.com>
2021-08-26 17:06:19 -0500

    Simple message of welcoming

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Simple message of welcoming
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """ first index template to getting started translate
+    """
+    title = 'Welcome to Holberton'
+    header = 'Hello world'
+    greeting = {'title': title, 'header': header}
+    return render_template('0-index.html', greeting=greeting)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', '5000')
