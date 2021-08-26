b6681090334de411fc0ff90c1dc8f6122c8447a6
Victor Zuluaga <zrvictor@outlook.com>
2021-08-26 18:03:50 -0500

    translate page

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -10,11 +10,12 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     """ config all objects to translate
     """
     LANGUAGES = ["en", "fr"]
 
+
 app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
-
