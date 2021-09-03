157369e57754835373547d6c2319a132b7c58d6f
Victor Zuluaga <zrvictor@outlook.com>
2021-09-03 17:33:38 -0500

    added new features

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Getting started using redis
 """
 import redis
-from typing import Union
+from typing import Union, Optional
 import uuid
 
 
@@ -27,3 +27,21 @@ class Cache:
         ID = str(uuid.uuid4())
         self._redis.mset({ID: data})
         return ID
+
+    def get(self, key: str, fn: Optional[callable] = None) \
+            -> Union[str, bytes, int, float]:
+        if fn:
+            value = self._redis.get(key)
+            return value.decode('utf-8')
+
+    def get_str(self, key: str) -> str:
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        v = self._redis.get(key)
+        try:
+            c = int(v.decode("utf-8"))
+        except Exception:
+            v = 0
+        return v
