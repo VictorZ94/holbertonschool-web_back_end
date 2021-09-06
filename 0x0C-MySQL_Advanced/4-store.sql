4dd7de1327b168ba0cae22a0c99abf36d4f57a3c
Victor Zuluaga <zrvictor@outlook.com>
2021-09-05 19:27:51 -0500

    store

---
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- store
+-- creates a trigger that decreases 
+CREATE TRIGGER orderdata
+BEFORE INSERT ON orders
+FOR EACH ROW UPDATE items 
+SET quantity = quantity - NEW.number 
+WHERE name = NEW.item_name;
