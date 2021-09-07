53c11f4d5ee9bce802a76fe69959f7c2ac1de452
Victor Zuluaga <zrvictor@outlook.com>
2021-09-06 22:50:50 -0500

    update topics

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -13,7 +13,8 @@ def update_topics(mongo_collection, name, topics):
     """
     mongo_collection.update_many(
         {"name": name},
-        {"$set": 
-            {"topics": topics}
+        {
+            "$set":
+                {"topics": topics}
         }
     )
