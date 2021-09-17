0012f66934c9fad04a9e23e8b324d319249b6ac6
Victor Zuluaga <zrvictor@outlook.com>
2021-09-16 20:12:29 -0500

    using getter and setter js

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -5,33 +5,33 @@ export default class HolbertonCourse {
     this.students = students;
   }
 
-  get getName() {
+  get name() {
     return this._name;
   }
 
-  set setName(name) {
+  set name(name) {
     if (typeof (name) !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
 
-  get getLenth() {
+  get length() {
     return this._length;
   }
 
-  set setLength(length) {
+  set length(length) {
     if (typeof (length) !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
 
-  get getStudents() {
+  get students() {
     return this._students;
   }
 
-  set setStudents(students) {
+  set students(students) {
     if (Array.isArray(students) && students.every((s) => typeof s === 'string')) {
       this._students = students;
     } else {
