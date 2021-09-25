2dd1d074d0936ae4e13c6864bf65830d042357c5
Victor Zuluaga <zrvictor@outlook.com>
2021-09-24 20:38:51 -0500

    process stdin and stdout streams

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const read = process.stdin.read();
+  if (read !== null) {
+    process.stdout.write(`Your name is: ${read}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
